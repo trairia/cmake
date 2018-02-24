a99939b93d16149d59a9a17553a2666937429a50
Kazunori Kimura <kazunori.abu@gmail.com>
2018-02-25 01:16:09 +0900

    gtest.cmake
      * refactor
      * Check CMAKE_THREAD_LIBS_INIT for MSVC

--- gtest.cmake
+++ gtest.cmake
@@ -6,15 +6,11 @@ find_package(Threads REQUIRED)
 
 include(ExternalProject)
 
-set(GTEST_PREFIX
-  ${PROJECT_BINARY_DIR}/googletest
-  )
-
 ExternalProject_Add(
   googletest
-  URL https://github.com/google/googletest/archive/release-1.8.0.zip
   PREFIX ${CMAKE_CURRENT_BINARY_DIR}/googletest
-
+  URL "https://github.com/google/googletest/archive/master.zip"
+  CMAKE_ARGS -DCMAKE_BUILD_TYPE=Release
   INSTALL_COMMAND ""
 )
 
@@ -29,26 +25,24 @@ include_directories(
   )
 
 # macro to define imported target
-macro(define_imported_target target library)
-  add_library(${target} IMPORTED STATIC GLOBAL)
+macro(define_imported_target target libpath)
+    add_library(${target} IMPORTED STATIC GLOBAL)
   add_dependencies(${target} googletest)
   set_target_properties(${target} PROPERTIES
-    IMPORTED_LOCATION ${library}
-    IMPORTED_LINK_INTERFACE_LIBRARIES ${CMAKE_THREAD_LIBS_INIT}
+    IMPORTED_LOCATION ${libpath}/${CMAKE_STATIC_LIBRARY_PREFIX}${target}${CMAKE_STATIC_LIBRARY_SUFFIX}
+  )
+  if (CMAKE_THREAD_LIBS_INIT)
+    set_target_properties(${target} PROPERTIES
+      IMPORTED_LINK_INTERFACE_LIBRARIES ${CMAKE_THREAD_LIBS_INIT}
     )
+  endif()
 endmacro(define_imported_target)
 
 # create imported target
-set(LIBRARIES
-  ${binary_dir}/googlemock/gtest/${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}
-  ${binary_dir}/googlemock/gtest/${CMAKE_STATIC_LIBRARY_PREFIX}gtest_main${CMAKE_STATIC_LIBRARY_SUFFIX}
-  ${binary_dir}/googlemock/${CMAKE_STATIC_LIBRARY_PREFIX}gmock${CMAKE_STATIC_LIBRARY_SUFFIX}
-  ${binary_dir}/googlemock/${CMAKE_STATIC_LIBRARY_PREFIX}gmock_main${CMAKE_STATIC_LIBRARY_SUFFIX}
-  )
+foreach(lib gtest gtest_main)
+  define_imported_target(${lib} ${binary_dir}/googlemock/gtest)
+endforeach()
 
-foreach(lib ${LIBRARIES})
-  get_filename_component(target_lib "${lib}" NAME)
-  string(REGEX REPLACE "^${CMAKE_STATIC_LIBRARY_PREFIX}" "" wo_pref ${target_lib})
-  string(REGEX REPLACE "${CMAKE_STATIC_LIBRARY_SUFFIX}$" "" target ${wo_pref})
-  define_imported_target(${target} ${lib})
+foreach(lib gmock gmock_main)
+  define_imported_target(${lib} ${binary_dir}/googlemock)
 endforeach()
